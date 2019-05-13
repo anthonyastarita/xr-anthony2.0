f8e2752e48569b0120cb2438047bb7cd2e809d77
Anthony Astarita <anthonyastarita98@gmail.com>
2019-05-13 12:37:39 -0400

    Initital Commit
    Unity Scene utilizing Photon and Lean Touch to demonstrate two users viewing the same object in 3D space

---
+++ PhotonTest/Assets/PlacenoteMultiplayerKit/Examples/ProteinStructureController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Placenote;
+
+public class ProteinStructureController : MonoBehaviour
+{
+    void Start()
+    {
+        // Register this object to the current game controller.
+        // This is important so that all clients have a reference to this object.
+        //GameController.Instance.RegisterMoon(this);
+    }
+}
---
+++ PhotonTest/Assets/Scripts/GameControllerTutorial.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Placenote;
+
+public class GameControllerTutorial : PlacenotePunMultiplayerBehaviour
+{
+    protected override void OnGameStart()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+        PhotonNetwork.Instantiate("SampleMoon", Vector3.zero, Quaternion.identity, 0);
+        PhotonNetwork.Instantiate("ProteinStructureParent", Vector3.zero, Quaternion.identity, 0);
+
+    }
+}
